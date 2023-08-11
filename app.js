dff1bc3db6f5051d4efb655dd4011af8ced5d204
Dennis <dennislinneman33@gmail.com>
2023-08-11 11:48:34 -0400

    pushing homework

---
+++ app.js
@@ -0,0 +1,46 @@
+// 1. Declare ten different variables and assign them the value of a number.
+
+var num1 = 5;
+var num2 = 10;
+var num3 = 15;
+var num4 = 20;
+var num5 = 25;
+var num6 = 30;
+var num7 = 35;
+var num8 = 40;
+var num9 = 45;
+var num10 = 50;
+
+//2. Two times, console.log the addition of two of the variables you declared.
+
+console.log(num1 + num2);
+console.log(num3 + num4);
+
+//3. Two times, console.log the subtraction of two of the variables you declared.
+
+console.log(num5 - num6);
+console.log(num7 - num8);
+
+//4. Two times, console.log the multiplication of two of the variables you declared.
+
+console.log(num9 * num10);
+console.log(num1 * num2);
+
+//5. Two times, console.log the division of two of the variables you declared.
+
+console.log(num3 / num4);
+console.log(num5 / num6);
+
+//6.Two times, increments one of the variables you declared.
+
+num7++;
+console.log(num7);
+num8++;
+console.log(num8);
+
+//7. Two times, decrement one of the variables you have declared.
+
+num9--;
+console.log(num9);
+num10--;
+console.log(num10);
